c5a4ce472f92595ba02ffccf05f331515988ee77
deseveaux <bdeseveaux@gmail.com>
2023-01-22 17:32:10 +0100

    add error 404 sky

--- js/scripts.js
+++ js/scripts.js
@@ -50,6 +50,24 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+    var body=document.body;
+    setInterval(createStar,100);
+    function createStar(){
+      var right=Math.random()*500;
+      var top=Math.random()*screen.height;
+      var star=document.createElement("div");
+   star.classList.add("star")
+    body.appendChild(star);
+    setInterval(runStar,10);
+      star.style.top=top+"px";
+    function runStar(){
+      if(right>=screen.width){
+        star.remove();
+      }
+      right+=3;
+      star.style.right=right+"px";
+    }
+    } 
 
 });
 
