34f9b95e5b705e8afd87d802e1796791bc4b1073
deseveaux <bdeseveaux@gmail.com>
2023-03-13 04:32:11 +0100

    graph idea solved; error z-index for some project

--- js/scripts.js
+++ js/scripts.js
@@ -103,3 +103,10 @@ function downloadQrCode() {
         link.click();
     });
 }
+
+//About page
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
